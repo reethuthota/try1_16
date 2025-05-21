101f35419da832055345e2e3dfa470cacb077b4a
Reethu Thota <129527629+reethuthota@users.noreply.github.com>
2025-05-21 13:42:21 +0530

    commit2

--- hello.py
+++ hello.py
@@ -1,10 +1,3 @@
 print("we can do this woooohooooo lets do it!")
 a = input("")
 print(a)
-print("hello world how are you do")
-# is this glitchyyyy?
-# hmmm
-# lets seee
-b = 27 
-c  = 39
-print(b+c)
